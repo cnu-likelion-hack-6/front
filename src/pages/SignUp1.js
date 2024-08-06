b5de42f4b338ab09bac07a07398fa77c44c0602e
hyeon002 <hhj48770@naver.com>
2024-08-06 13:35:33 +0900

    api 추가

--- src/pages/SignUp1.js
+++ src/pages/SignUp1.js
@@ -16,7 +16,8 @@ function SignUp1() {
   const [ nextPage, setNextPage ] = useState('회원 가입하기');
 
   const signUp = () => {
-    fetch('https://melodic-valkyrie-f0a3b6.netlify.app/members', {
+    //fetch('https://melodic-valkyrie-f0a3b6.netlify.app/members', {
+    fetch('/api/members', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
